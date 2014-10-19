536bd54926021e66fad910cf4f867a461e7ad619
hyone <hyone.development@gmail.com>
2014-10-19 22:49:56 +0900

    Support CentOS 7

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures packages from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 depends          'build-essential'
+
+supports         'ubuntu', '12.04'
+supports         'centos', ">= 6"
