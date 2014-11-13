5bba455256af6a6061359cd415bcbce3e145b6e6
hyone <hyone.development@gmail.com>
2014-11-13 23:32:33 +0900

    Fix: version specification of 'supports'

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 depends          'build-essential'
 
-supports         'ubuntu', '12.04'
-supports         'centos', ">= 6"
+supports         'ubuntu', '>= 12.04'
+supports         'centos', ">= 6.0"
